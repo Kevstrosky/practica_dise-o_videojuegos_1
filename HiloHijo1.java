81bddbcfe5130de278746687c337af29e5ff3cf6
kevstrosky <kevinochoa.dev@gmail.com>
2023-01-25 11:49:30 -0700

    Segunda practica con Threads. Introducción a programación orientada a objetos con sets

--- HiloHijo1.java
+++ HiloHijo1.java
@@ -1,5 +1,23 @@
 public class HiloHijo1 extends Thread{
+
+    String name; 
+    int tiempo;
+
+    public void setNombre(String name){
+        this.name = name;
+    }
+
+    public void setTiempo(int tiempo){
+        this.tiempo = tiempo;
+    }
+
     public void run(){
-            System.out.println("Estoy en el Hilo Hijo1");
+            System.out.println("Hilo:"+name+""+Thread.currentThread());
+            for(int i = 1; i <= 10; i++){
+                try{
+                    System.out.println(i+"["+name+"]");
+                    Thread.sleep(tiempo);
+                }catch(Exception e){}
+            }
     }
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        Thread t = Thread.currentThread();
-        System.out.println("Hilo actual: " + t);
-
         HiloHijo1 h1 = new HiloHijo1();
-        h1.run();
-
-        try{
-            for(int i = 0; i < 10; i++){
-                System.out.print(".");
-                Thread.sleep(100);
-            }
-        }catch(Exception e){}
+        h1.setNombre("H1 a 1 segundo");
+        h1.setTiempo(1000);
+        h1.start();
+        HiloHijo1 h2 = new HiloHijo1();
+        h2.setNombre("H2 a 1.5 segundos");
+        h2.setTiempo(1500);
+        h2.start();
     }
 }
