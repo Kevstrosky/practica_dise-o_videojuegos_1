2015199f746bf5a1e6c660d7fe7c77dcc2796d11
kevstrosky <kevinochoa.dev@gmail.com>
2023-01-30 19:51:54 -0700

    Contador agregado. Se actualiza cada segundo en un  label

--- p1/Ventana.java
+++ p1/Ventana.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana extends JFrame {
     public Ventana(){
@@ -8,12 +10,31 @@ public class Ventana extends JFrame {
     private void initValues(){
 
         Font font1 = new Font("Arial", 1, 24);
-        //Hacer un botón que cambie este texto1 utilizando un Thread para que cambié el número del 1 al 10
+        Font font2 = new Font("Arial", 1, 12);
         JLabel texto1 = new JLabel("0");
+        JButton boton1 = new JButton("Iniciar contador");
+        ActionListener actionListener = new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+        new Thread(() -> {
+         for (int i = 1; i <= 10; i++) {
+            try {
+               Thread.sleep(1000);
+            } catch (InterruptedException e) {
+               e.printStackTrace();
+            }
+            texto1.setText(String.valueOf(i));
+         }
+      }).start();
+        }
+        };
+         boton1.addActionListener(actionListener);
         texto1.setFont(font1);
+        boton1.setFont(font2);
         texto1.setBounds(10,10,50,25);
+        boton1.setBounds(10,50,150,35);
 
         add(texto1);
+        add(boton1);
         setTitle("Mi ventana");
         setSize(300,300);
         setResizable(false);
