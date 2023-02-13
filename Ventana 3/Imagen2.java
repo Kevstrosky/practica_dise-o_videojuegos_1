8a964c1f826e1087871162bb99c18a006144604c
kevstrosky <kevinochoa.dev@gmail.com>
2023-02-13 11:58:21 -0700

    controlar a un personaje con las teclas

---
+++ Ventana 3/Imagen2.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Imagen2 extends JLabel implements Runnable, KeyListener {
+
+    private String url1, url2;
+    private ImageIcon icon;
+    private int posX = 10; 
+    private boolean runStatus = false;
+
+    public Imagen2(String url1, String url2) {
+        this.url1 = url1;
+        this.url2 = url2;
+
+        icon = new ImageIcon(this.getClass().getResource(url1));
+        setIcon(icon);
+    }
+
+public void run() {
+    runStatus = true;
+}
+
+public void keyTyped(KeyEvent ke) {}
+
+public void keyPressed(KeyEvent ke) {
+    if (runStatus) {
+            if ((posX % 2) != 0) {
+                icon = new ImageIcon(this.getClass().getResource(url2));
+            } else {
+                icon = new ImageIcon(this.getClass().getResource(url1));
+            }
+            setBounds(posX, 80,42,42);
+            setIcon(icon);
+        if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+            posX++;
+        }
+        if (ke.getKeyCode() == KeyEvent.VK_SHIFT) {
+            posX+=3;
+        }
+    }
+}
+
+public void keyReleased(KeyEvent ke) {}
+
+}
